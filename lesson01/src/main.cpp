5f726a1075dadf984c54a8456eed3e980a7208f5
perinn <dimaprein23@gmail.com>
2021-09-15 12:32:24 +0300

    aboba

--- lesson01/src/main.cpp
+++ lesson01/src/main.cpp
@@ -3,6 +3,7 @@
 
 // таким образом подключаются наши функции
 #include "simple_sum.h"
+#include "some_math.h"
 
 int main() {
     // таким образом выводятся сообщения в консоль
@@ -22,7 +23,7 @@ int main() {
     std::cout << "a+b=" << a << "+" << b << "=" << res << std::endl;
 
     // TODO 06 выведите в консоль чему равно fibbonachiFast(b), не забудьте что нужно добавить не хватающий инклюд - some_math.h в которой объявлена эта функция
-    // std::cout << "fib(b)=" <<
+    std::cout << "fib(b)=" << fibbonachiFast(b) << std::endl;
 
     std::vector<double> values;
     std::cout << "values size: " << values.size() << std::endl;
@@ -35,16 +36,27 @@ int main() {
 
     // TODO 07 выведите в консоль каждый элемент из динамического массива
     for (int i = 0; i < values.size(); ++i) {
-        double x = values[i];
+        std::cout << values[i] << std::endl;
     }
 
     // TODO 08 считывайте числа из консоли (и добавляйте их в вектор) до тех пор пока не будет введен ноль, после чего просуммируйте считанные числа и выведите сумму
-    // while () {
-    //     ...
-    //     if (...) {
-    //         break;
-    //     }
-    // }
+    while (true) {
+         double x;
+         std::cin >> x;
+         if (x==0) {
+             break;
+         }
+         else{
+             values.push_back(x);
+         }
+     }
+
+    double ans = 0;
+    for(double i : values){
+        ans += i;
+    }
+
+    std::cout << "sum of values = " << ans << std::endl;
 
     return 0;
 }
--- lesson01/src/some_math.cpp
+++ lesson01/src/some_math.cpp
@@ -1,11 +1,40 @@
 #include "some_math.h"
+#include <limits.h>
+#include <float.h>
+#include <math.h>
+
 
 int fibbonachiRecursive(int n) {
+    if (n==0){
+        return 0;
+    }
+    if(n==1){
+        return 1;
+    }
+    else{
+        return fibbonachiRecursive(n-1)+ fibbonachiRecursive(n-2);
+    }
     // TODO 03 реализуйте функцию которая считает числа Фибоначчи - https://ru.wikipedia.org/wiki/%D0%A7%D0%B8%D1%81%D0%BB%D0%B0_%D0%A4%D0%B8%D0%B1%D0%BE%D0%BD%D0%B0%D1%87%D1%87%D0%B8
     return 0;
 }
 
 int fibbonachiFast(int n) {
+    std::vector<int> xs;
+    xs.push_back(0);
+    xs.push_back(1);
+    if(n==0){
+        return xs[0];
+    }
+    if(n==1){
+        return xs[2];
+    }
+    else{
+        for(int i =2; i <= n; i++){
+            xs.push_back(xs[i-1]+xs[i-2]);
+        }
+    }
+    return xs[n];
+
     // TODO 04 реализуйте быструю функцию Фибоначчи с использованием std::vector
     return 0;
 }
@@ -13,8 +42,20 @@ int fibbonachiFast(int n) {
 double solveLinearAXB(double a, double b) {
     // TODO 10 решите линейное уравнение a*x+b=0 а если решения нет - верните наибольшее значение double - найдите как это сделать в интернете по запросу "so cpp double max value" (so = stackoverflow = сайт где часто можно найти ответы на такие простые запросы), главное НЕ КОПИРУЙТЕ ПРОСТО ЧИСЛО, ПОЖАЛУЙСТААаа
     // если решений сколь угодно много - верните максимальное значение со знаком минус
-    double x = 0.0;
-    return x;
+    if(a==0){
+        if(b==0){
+            return -1*DBL_MAX;
+        }
+        else{
+            return DBL_MAX;
+        }
+    }
+
+    else{
+        return -b/a;
+    }
+
+    return 0;
 }
 
 std::vector<double> solveSquare(double a, double b, double c) {
@@ -22,6 +63,18 @@ std::vector<double> solveSquare(double a, double b, double c) {
     // если корня два - они должны быть упорядочены по возрастанию
     std::vector<double> results;
     // чтобы добавить в вектор элемент - нужно вызвать у него метод push_back:
-    results.push_back(23.9);
+    if(a!=0) {
+        double D = b * b - 4 * a * c;
+        if (D == 0) {
+            results.push_back((-b) / (2 * a));
+        }
+        if (D > 0) {
+            results.push_back((-b - sqrt(D)) / (2 * a));
+            results.push_back((-b + sqrt(D)) / (2 * a));
+        }
+    }
+    else{
+        results.push_back(solveLinearAXB(b,c));
+    }
     return results;
 }
