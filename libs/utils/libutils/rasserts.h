4343b881093d27ae3d947730341275ae0ed5e403
perinn <dimaprein23@gmail.com>
2021-09-15 12:20:56 +0300

    aboba

--- libs/utils/libutils/rasserts.h
+++ libs/utils/libutils/rasserts.h
@@ -5,4 +5,9 @@
 
 int debugPoint(int line);
 
-#define rassert(condition, message) if (!(condition)) { std::stringstream ss; (ss << "Assertion \"" << message << "\" failed at line " << debugPoint(__LINE__) << "!"); throw std::runtime_error(ss.str()); }
+#define rassert(condition, message) \
+if (!(condition)) {                 \
+    std::stringstream ss;           \
+    (ss << "Assertion \"" << message << "\" failed at line " << debugPoint(__LINE__) << "!"); \
+    throw std::runtime_error(ss.str());                                                       \
+}
