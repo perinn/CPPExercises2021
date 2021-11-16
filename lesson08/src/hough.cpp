98c405d24143fe2da063009ce58f45d32c1e3fe3
perinn <dimaprein23@gmail.com>
2021-11-16 22:13:30 +0300

    changed

--- lesson08/src/hough.cpp
+++ lesson08/src/hough.cpp
@@ -203,38 +203,28 @@ std::vector<PolarLineExtremum> filterStrongLines(std::vector<PolarLineExtremum> 
 
 std::vector<PolarLineExtremum> CutLines(std::vector<PolarLineExtremum> arr0, cv::Mat img){
     std::vector<PolarLineExtremum> arr1;
-    bool aboba = false;
-    for(int i = 0 ; i < arr0.size(); ++i){
-        std::vector<PolarLineExtremum> arri;
-        for(int j = 0; j < arr0.size(); ++j){
-            if(i<arr0.size()&&j<arr0.size()){
-            if(fabs(arr0[i].r-arr0[j].r)<0.1*fmin(img.cols, img.rows)&&fabs(arr0[i].theta-arr0[j].theta)<5){
-                aboba = true;
-                arri.push_back(arr0[j]);
-                if(i!=j){
-                    arr0.erase(arr0.begin()+j);
+    bool a = false;
+    for(int i = 0; i < arr0.size(); i++){
+        for(int j = 0; j < arr0.size(); j++){
+            if(i!=j && i < arr0.size() && j < arr0.size()) {
+                if (fabs(arr0[i].theta-arr0[j].theta)<5 && fabs(arr0[i].r - arr0[j].r)<0.1*fmin(img.cols, img.rows)){
+                    a = true;
+                    double theta0 = (arr0[i].theta+arr0[j].theta)/2;
+                    double r0 = (arr0[i].r+arr0[j].r)/2;
+                    double votes0 = (arr0[i].votes+arr0[j].votes)/2;
+
+                    arr0.erase(arr0.begin()+j-1);
+                    arr0.erase(arr0.begin()+i-1);
+
+                    arr1.emplace_back(theta0, r0, votes0);
                 }
-            }}
+            }
         }
-        float r0 = 0;
-        float theta0 = 0;
-        float votes0 = 0;
-        for(int x = 0; x < arri.size(); ++x){
-            r0 += arri[x].r;
-            theta0 += arri[x].theta;
-            votes0 += arri[x].votes;
-        }
-        r0 /= arri.size();
-        theta0 /= arri.size();
-        votes0 /= arri.size();
-
-        arr1.push_back(PolarLineExtremum(theta0,r0,votes0));
-
     }
-    if(aboba){
+    arr1.insert( arr1.end(), arr0.begin(), arr0.end() );
+    if(a){
         return CutLines(arr1, img);
-    }
-    else {
+    }else{
         return arr1;
     }
 }
