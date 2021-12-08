95c4ef3e37a836e03b676d82abea0ae571917786
perinn <dimaprein23@gmail.com>
2021-12-08 12:31:19 +0300

    changed

--- lesson11/src/parseSymbols.cpp
+++ lesson11/src/parseSymbols.cpp
@@ -1,8 +1,18 @@
+#include <opencv2/imgproc.hpp>
 #include "parseSymbols.h"
+#include <filesystem>
+#include <iostream>
+#include <libutils/rasserts.h>
+
+#include "parseSymbols.h"
+
+#include <opencv2/imgproc.hpp>
+
 
 std::vector<cv::Mat> splitSymbols(cv::Mat img)
 {
     std::vector<cv::Mat> symbols;
+    cv::Mat img1 = cv::adaptiveThreshold(img, 255.0, 'BORDER_REPLICATE' ,'THRESH_BINARY', 64, 1.0);
     // TODO 101: чтобы извлечь кусок картинки (для каждого прямоугольника cv::Rect вокруг символа) - загуглите "opencv how to extract subimage"
     return symbols;
 }
